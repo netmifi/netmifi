6d11cf942b56174e534e5bc6aad8f6ee55091e07
Okenwa victor <OkenwaVictor003@gmail.com>
2024-03-15 20:38:47 +0100

    Setting up redux, scss and adding netmifi icons

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 // Our Easy peasy store for global, constants and reusable functions
 import { action, createStore } from 'easy-peasy'
-import netmifiLogo from "/public/netmifi-logo.png" // i moved the logo to the public folder so that it can be accesed easily just by one variable
+import netmifiLogo from "/netmifi-logo.png" // i moved the logo to the public folder so that it can be accesed easily just by one variable
 
 export default createStore({
     logo: netmifiLogo,
