8dffc0e780a7b70b4af4482648c679c27afc7f77
Emereh-boop <abuashiaemereh@gmail.com>
2025-03-13 08:55:59 +0100

    integrated the frontend for netmifi waitlist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,49 +2,10 @@ import { ThemeProvider } from "@/app/theme-provider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Courses from "./pages/Courses";
-import Blogs from "./pages/Blogs";
-import Blog from "./layouts/blogs/Blog";
-import TopCourses from "./layouts/courses/TopCourses";
-import RecentCourses from "./layouts/courses/RecentCourses";
-import FeaturedBlogs from "./layouts/blogs/FeaturedBlogs";
-import PopularBlogs from "./layouts/blogs/PopularBlogs";
-import RecentBlogs from "./layouts/blogs/RecentBlogs";
-import Instructors from "./pages/Instructors";
-import CoursePreview from "./layouts/courses/CoursePreview";
-import AppOutletLayout from "./layouts/AppOutletLayout";
-import AuthOutletLayout from "./layouts/AuthOutletLayout";
-import LearnPlay from "./layouts/courses/LearnPlay";
-import About from "./pages/About";
-import MyCourses from "./layouts/courses/MyCourses";
+import Community from "./pages/Community";
 import { Toaster } from "@/components/ui/sonner";
-import Instructor from "./layouts/instructors/Instructor";
-import Contact from "./pages/Contact";
-import Help from "./pages/Help";
-import Feedback from "./pages/Feedback";
-import SignUp from "./pages/auth/SignUp";
-import SignIn from "./pages/auth/SignIn";
-import OTPVerification from "./pages/auth/OTPVerification";
-import ForgotPassword from "./pages/auth/forgot-password/ForgotPassword";
-import ViaEmail from "./pages/auth/forgot-password/ViaEmail";
-import NewPassword from "./pages/auth/forgot-password/NewPassword";
-import Welcome from "./pages/auth/welcome/Welcome";
-import Interest from "./pages/auth/welcome/Interest";
-import Profile from "./pages/account/Profile";
-import Settings from "./pages/account/Settings";
-import SignInstructor from "./pages/auth/welcome/SignInstructor";
-import TestPage from "./pages/TestPage";
-// import InstructorDashboardOutletLayout from "./layouts/InstructorDashboardOutletLayout";
 
-import InstructorDashboard from "./pages/instructor_dashboard/Dashboard";
 import { AppProvider } from "./app/app-provider";
-import InstructorDashboardOutletLayout from "./layouts/InstructorDashboardOutletLayout";
-import MyEarnings from "./pages/instructor_dashboard/MyEarnings";
-import Students from "./pages/instructor_dashboard/Students";
-import CertifiedStudents from "./pages/instructor_dashboard/CertifiedStudents";
-import Followers from "./pages/instructor_dashboard/Followers";
-import DashboardCourses from "./pages/instructor_dashboard/Courses";
-import CreateCourse from "./pages/instructor_dashboard/CreateCourse";
 
 const App = () => {
   return (
@@ -52,100 +13,11 @@ const App = () => {
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <Router>
           <Routes>
-            {/* AUTH ROUTES */}
-            <Route path="/auth" element={<AuthOutletLayout />}>
-              <Route path="sign-in" element={<SignIn />} />
-              <Route path="sign-up" element={<SignUp />} />
-              <Route path="otp-verification" element={<OTPVerification />} />
-              {/* <Route path="verification-type" element={<OTPVerification />} /> */}
-              <Route path="forgot-password" element={<ForgotPassword />}>
-                <Route path="via-email" element={<ViaEmail />} />
-                <Route path="new-password" element={<NewPassword />} />
-              </Route>
-
-              <Route path={"welcome"} element={<Welcome />}>
-                <Route
-                  path="instructor-application"
-                  element={<SignInstructor />}
-                />
-                <Route path={"interest"} element={<Interest />} />
-              </Route>
-            </Route>
-
-            {/* ROOT ROUTES */}
-
-            <Route path="/" element={<AppOutletLayout />}>
+              <Route path="index" element={<Home />} />
               <Route path="home" element={<Home />} />
-
-              <Route path="contact" element={<Contact />} />
-
-              {/* COURSES ROUTE */}
-              <Route path="courses" element={<Courses />}>
-                <Route path="top" element={<TopCourses page="self" />} />
-                <Route path="recent" element={<RecentCourses page="self" />} />
-                <Route path="course/:id" element={<CoursePreview />} />
-                <Route path="my-courses" element={<MyCourses />} />
-                <Route path="my-courses/:id" element={<LearnPlay />} />
-              </Route>
-
-              {/* COURSES ROUTE */}
-
-              <Route path="pricing" element={<h1>Pricing</h1>} />
-
-              {/* BLOGS ROUTE */}
-              <Route path="blogs" element={<Blogs />}>
-                <Route
-                  path="featured"
-                  element={<FeaturedBlogs page="self" />}
-                />
-                <Route path="popular" element={<PopularBlogs page="self" />} />
-                <Route path="recent" element={<RecentBlogs page="self" />} />
-                <Route path="blog/:id" element={<Blog />} />
-              </Route>
-
-              {/* INSTRUCTORS ROUTE */}
-              <Route path="instructors" element={<Instructors />}>
-                <Route path="instructor/:id" element={<Instructor />} />
-              </Route>
-
-              {/* USER ROUTES */}
-              <Route path="/user/:id" element={<h1>User</h1>} />
-              <Route
-                path="/user/:id/courses"
-                element={<h1>Recent Courses</h1>}
-              />
-
-              {/* HELP ROUTE */}
-              <Route path="/help" element={<Help />} />
-              <Route path="/t&c" element={<TestPage />} />
-              <Route path="/test" element={<TestPage />} />
-
-              <Route path="/account/profile" element={<Profile />} />
-              <Route path="/account/settings" element={<Settings />} />
-
-              {/* FEEDBACK ROUTE */}
-              <Route path="/feedback" element={<Feedback />} />
-              {/* INSTRUCTOR DASHBOARD ROUTES */}
-              <Route
-                path="dashboard"
-                element={<InstructorDashboardOutletLayout />}
-              >
-                <Route path="home" element={<InstructorDashboard />} />
-                <Route path="home/students" element={<Students />} />
-                <Route
-                  path="home/certified-students"
-                  element={<CertifiedStudents />}
-                />
-                <Route path="home/followers" element={<Followers />} />
-                <Route path="home/courses" element={<DashboardCourses />} />
-                <Route path="my-earnings" element={<MyEarnings />} />
-                <Route path="create" element={<CreateCourse />} />
-              </Route>
-
-              {/* 404 page ROUTES */}
+              <Route path="community" element={<Community />} />
               <Route path="*" element={<NotFound />} />
               <Route path="404" element={<NotFound />} />
-            </Route>
           </Routes>
           <Toaster />
         </Router>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,165 +1,179 @@
-import { useApp } from "@/app/app-provider";
-import { HomeSvg } from "@/assets/svg";
-import CourseCarousel from "@/components/courses/CourseCarousel";
-import MyCourseCarousel from "@/components/courses/my-courses/MyCourseCarousel";
-import InstructorCard from "@/components/instructors/InstructorCard";
-import Jumbotron from "@/components/Jumbotron";
-import Newsletter from "@/components/Newsletter";
-import PostAvatar from "@/components/PostAvatar";
-import { Button } from "@/components/ui/button";
-import {
-  tempPurchasedCourses as purchasedCourses,
-  tempInstructors as instructors,
-  tempCourses as courses,
-} from "@/constants/temp";
-import RecentCourses from "@/layouts/courses/RecentCourses";
-import TopCourses from "@/layouts/courses/TopCourses";
-import { cn, convertToReadableNumber } from "@/lib/utils";
-import { ArrowDownIcon } from "lucide-react";
-import { useRef, useState } from "react";
-import { NavLink } from "react-router-dom";
-// import CountUp from "react-countup";
+import { useState } from "react";
+import { AiFillCheckCircle, AiFillHome } from "react-icons/ai";
+import { FaTimes } from "react-icons/fa"; // Cancel icon
+import { FaRegCheckCircle } from "react-icons/fa"; // Done (red) button icon
+import { FaPeopleGroup } from "react-icons/fa6";
+import Logo from "../assets/images/netmifi_logo.svg";
+import { Menu, X } from "lucide-react";
+import NavWaitlist from "@/components/navbar/NavbarWaitlist";
 
 const Home = () => {
-  const { isAuth } = useApp();
-  const greetingPhrases = [
-    "Ahoy There",
-    "Hi There",
-    "Hello There",
-    "Good to See You",
-    "Welcome Back",
-  ];
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const greeting =
-    greetingPhrases[Math.round(Math.random() * (greetingPhrases.length - 1))];
-
-  const exploreSectionRef = useRef<HTMLElement>(null);
-  const handleHandleExplore = () => {
-    exploreSectionRef?.current?.scrollIntoView({ behavior: "smooth" });
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    // Simulate a successful form submission
+    try {
+      // Show the modal on success
+      setIsModalOpen(true);
+    } catch (err) {
+      alert("Error enlisting. Please try again.");
+    }
   };
-  const [isCountEnded, setIsCountEnded] = useState(false);
-  const netmifiNumbers = [
-    {
-      count: 152020,
-      label: "students",
-    },
-    {
-      count: 28020,
-      label: "online courses",
-    },
-    {
-      count: 6820,
-      label: "certified",
-    },
-  ];
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <div className="flex flex-col gap-16">
-      {isAuth && (
-        <section className="padding-x padding-y flex items-center gap-3">
-          <PostAvatar
-            isVerified={false}
-            profileName="Metane Ekeh"
-            profileURL=""
-            profileImage=""
-            profileImageClassName="size-20 text-2xl"
-            onlyAvatar
-          />
+    <div className="bg-default w-auto lg:w-dvw h-dvh relative">
+      <div className="absolute flex z-0 items-center justify-center min-h-dvh bg-white/90 top-0 right-0 left-0 bottom-0"></div>
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+          <div className="bg-white rounded-xl p-6 w-[70%] md:w-[25%] space-y-5">
+            {/* Modal Header with Close Icon */}
+            <div className="flex justify-end">
+              <button
+                onClick={closeModal}
+                className="text-black p-1 rounded-full bg-gray-300"
+              >
+                <FaTimes size={18} />
+              </button>
+            </div>
 
-          <div className="flex flex-col gap-px justify-center">
-            <h2 className="text-2xl sm:text-3xl font-bold">{greeting},</h2>
-            <p>FirstName, LastName</p>
-          </div>
-        </section>
-      )}
+            {/* Modal Content */}
+            <div className="flex flex-col items-center gap-5">
+              {/* Green Check Icon */}
+              <div className="text-[#1B6909]">
+                <AiFillCheckCircle size={120} />
+              </div>
+              <p className="text-center text-xl max-w-72 text-[#0F2816] font-bold">
+                We’ve added you to our waiting list!
+              </p>
+              <p className="text-center text-xs max-w-60 text-[#000000B2]/70">
+                Thanks for joining our waiting list, we’ll let you know when
+                Netmifi is ready.
+              </p>
+            </div>
 
-      <Jumbotron
-        className="bg-secondary"
-        image={HomeSvg}
-        title="Unlock the creator in you"
-        titleClassName="sm:text-5xl"
-        body="Learn. Create. Share"
-        button={
-          <div className="flex flex-col gap-4">
-            <Button
-              className="rounded-full text-lg mr-auto"
-              onClick={handleHandleExplore}
-            >
-              Explore <ArrowDownIcon />
-            </Button>
-
-            <div className="flex gap-3">
-              {netmifiNumbers.map((item) => (
-                <div key={item.label} className="flex flex-col items-center">
-                  <h4 className="w-full  text-center font-bold text-xl pb-px border-b-2 border-b-gray-500">
-                    {/* <CountUp
-                      enableScrollSpy
-                      duration={5}
-                      scrollSpyDelay={2}
-                      end={item.count}
-                      start={0}
-                      startOnMount
-                      scrollSpyOnce
-                      onEnd={() => {
-                        setIsCountEnded(true);
-                        console.log("ended");
-                      }}
-                      className={cn({ hidden: isCountEnded })}
-                    /> */}
-
-                    {isCountEnded && (
-                      <>{convertToReadableNumber(item.count) + "+"}</>
-                    )}
-                  </h4>
-                  <p className="font-montserrat">{item.label}</p>
-                </div>
-              ))}
+            {/* Done Button */}
+            <div className="flex justify-center">
+              <button
+                className="bg-[#9E0000] hover:bg-[#CF8080] text-white py-3 justify-center w-5/6 text-sm rounded-xl flex items-center gap-2"
+                onClick={closeModal}
+              >
+                <FaRegCheckCircle size={16} />
+                Done
+              </button>
             </div>
           </div>
-        }
-      />
-
-      <section
-        className="padding-x padding-y flex flex-col gap-16"
-        ref={exploreSectionRef}
-      >
-        {isAuth && (
-          <div>
-            <MyCourseCarousel data={purchasedCourses} />
-          </div>
-        )}
-
-        <TopCourses page="child" />
-        <RecentCourses page="child" />
-
-        <div className="flex flex-col gap-7">
-          <div className="flex flex-col gap-2">
-            <h3 className="font-semibold">Who you will learn from</h3>
-            <p className="font-montserrat">
-              We have a community of elite and experienced instructors that
-              would make you excel in your chosen career path.
-            </p>
-          </div>
-
-          <div className="flex flex-wrap justify-around gap-6">
-            {instructors.map((instructor) => (
-              <InstructorCard key={instructor.id} instructor={instructor} />
-            ))}
-          </div>
-          <Button asChild className="mx-auto mt-5 rounded-full">
-            <NavLink to="/instructors">See more</NavLink>
-          </Button>
+        </div>
+      )}
+      <NavWaitlist/>
+      {/* <div className="justify-between bg-white hidden lg:flex lg:h-80px lg:w-[833px] lg:px-[105px] lg:py-[30px] rounded-3xl ring-[0.3px] fixed top-[43px] left-[49px]">
+        <div className="w-[133px] h-[39px]">
+          <img src={Logo} alt="Logo netmifi" />
         </div>
 
-        {isAuth && <CourseCarousel title="top picks for you" data={courses} />}
-
-        {isAuth && (
-          <div className="flex flex-col gap-7">
-            <CourseCarousel title="quick and easy" data={courses} />
+        <div className="flex gap-4 items-center">
+          <div className="flex items-center gap-2 ">
+            <AiFillHome fill="#000000" size={20} />
+            <a href="/home">Home</a>
           </div>
-        )}
-        <Newsletter />
-      </section>
+          <div className="flex items-center gap-2 w-[135px] h-[26px]">
+            <FaPeopleGroup size={20} />
+            <a href="/community">Community</a>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white lg:hidden w-full lg:px-[105px] px-10 py-[30px] lg:ring-[0.3px] fixed space-y-4 ">
+        <div className="justify-between flex r">
+          <div className="w-[133px] h-[39px]">
+            <img src={Logo} alt="Logo netmifi" />
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <Menu /> <X />
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-2 ">
+            <AiFillHome fill="#000000" size={20} />
+            <a href="/home">Home</a>
+          </div>
+          <div className="flex items-center gap-2 w-[135px] h-[26px]">
+            <FaPeopleGroup size={20} />
+            <a href="/community">Community</a>
+          </div>
+        </div>
+      </div> */}
+      <div className="mt-36 lg:mt-56 flex justify-center lg:space-x-10">
+        <div className="lg:flex flex-col hidden justify-between py-10 text-white h-[60vh]">
+          <div className="bg-[#1DA1F2] px-2 text-center rounded-lg text-lg -skew-x-[9.62deg] skew-y-[9.62deg]">
+            Adventurous
+          </div>
+
+          <div className="bg-[#A66CFF] px-2 text-center rounded-lg text-lg skew-x-[7.96deg] -skew-y-[7.96deg]">
+            Community-driven
+          </div>
+        </div>
+        <div className="flex flex-col items-start lg:items-center gap-4 z-10 p-5 lg:px-0">
+          <h1 className="text-3xl lg:text-[2.05rem] font-bold lg:text-center lg:max-w-[700px]">
+            THE ULTIMATE LAUNCHPAD FOR ADDICTIVE LEARNING AND SOCIALIZING!
+          </h1>
+          <p className="lg:max-w-[650px] lg:text-center text-[#1A0F28] text-medium lg:text-lg">
+            Get exclusive access to our groundbreaking All-in-One e-learning and
+            social commerce platform for creators! Join our launch list to be
+            among the first to try it out and shape the future of edtech.
+          </p>
+
+          <form onSubmit={handleAdd} className="lg:w-[534px] w-full space-y-5">
+            <p className="font-bold text-xl">JOIN OUR WAITLIST</p>
+            <div className="px-5 flex flex-col py-2 ring-[1px] ring-black rounded-xl hover:ring-[#9E0000]">
+              <label className="md:text-sm text-[#CE2600]" htmlFor="name">
+                Full name
+              </label>
+              <input
+                className="outline-none text-base text-[#1A0F28] "
+                type="text"
+                name="name"
+                id="name"
+                required
+                placeholder="Enter your full name"
+              />
+            </div>
+            <div className="px-5 flex flex-col py-2 ring-[1px] ring-black rounded-xl hover:ring-[#9E0000]">
+              <label className="md:text-sm text-[#CE2600]" htmlFor="name">
+                Email
+              </label>
+              <input
+                className="outline-none text-base text-[#1A0F28]"
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="Example@gmail.com"
+              />
+            </div>
+            <button
+              className="px-5 w-full bg-[#9E0000] hover:bg-[#CF8080] text-white flex justify-center py-4 ring-[1px] ring-[#CF8080] rounded-xl"
+              type="submit"
+            >
+              Join Waitlist
+            </button>
+          </form>
+        </div>
+        <div className="lg:flex flex-col hidden justify-between py-10 text-white h-[60vh]">
+          <div className="bg-[#84C50E] px-4 text-center rounded-lg text-lg skew-x-[7.96deg] -skew-y-[7.96deg]">
+            Growth
+          </div>
+
+          <div className="bg-[#FF5000] px-4 text-center rounded-lg text-lg -skew-x-[9.62deg] skew-y-[9.62deg]">
+            Rewarding
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
